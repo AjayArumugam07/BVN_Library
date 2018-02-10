8ccd2becf0506eabb2e1cbd029196c788c7cd9c2
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2018-02-09 23:53:17 -0600

    11 Starting Code for Drawer Menu

--- BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/MainActivity.java
+++ BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/MainActivity.java
@@ -6,13 +6,24 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TODO comment line 17 and uncomment line 18 to test auth screen
         setContentView(R.layout.drawer_menu);
+        //startActivity(new Intent(this, AuthScreen.class));
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerMenu);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        //drawerLayout.addDrawerListener(toggle);
+        //toggle.syncState();
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
 
