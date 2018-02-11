ccca61b35febd979f76238b4d904a41e168a510e
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2018-02-11 14:00:56 -0600

    14 Adding Theme Color and Layouts for Drawer Menu Options

---
+++ BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/Librarians.java
@@ -0,0 +1,13 @@
+package com.imaginationcreators.bvnlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Librarians extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.librarians_screen);
+    }
+}
