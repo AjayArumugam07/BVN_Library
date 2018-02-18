c13d2917721ce286cc01d957987bccc221bd4817
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2018-02-17 22:50:25 -0600

    17 Fixing Spinner Layout and Adding Code for Search and Spinner

--- BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/HomeScreen.java
+++ BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/HomeScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
@@ -20,11 +21,30 @@ public class HomeScreen extends DrawerMenu {
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.frameContent);
         getLayoutInflater().inflate(R.layout.home_screen, contentFrameLayout);
 
+
         searchView = (SearchView) findViewById(R.id.searchBar);
         spinner = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(HomeScreen.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.spinnerNames));
         listAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(listAdapter);
+        searchView.setOnQueryTextListener(searchListener);
     }
+
+    SearchView.OnQueryTextListener searchListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            // TODO call search class with query as search string and searchBy as search parameter
+
+            // Will either be Title, Author, or ISBN
+            String searchBy = spinner.getSelectedItem().toString();
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            // TODO update search suggestions here
+            return false;
+        }
+    };
 }
