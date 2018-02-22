b1bae5657ca96ac19a70e84cb26bb5e25b0e90e9
LAPTOP-ENO5PSQ6\Nirvan <nirvan.silswal@gmail.com>
2018-02-21 21:44:58 -0600

    22 Click map in the drawer and the map will be pulled up!

--- BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/DrawerMenu.java
+++ BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/DrawerMenu.java
@@ -1,5 +1,6 @@
 package com.imaginationcreators.bvnlibrary;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
@@ -29,17 +30,44 @@ public class DrawerMenu extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.setDrawerListener(toggle);
 
+
+
+
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.map:
+                        Intent i = new Intent(DrawerMenu.this, Map.class);
+                        startActivity(i);
+                        break;
+
+
+                }
                 return false;
+
             }
+
+            /*
+            Rajin and Ajay to add the click on the map to bring it up, i had to get rid of this
+
+            protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+                super.onPostCreate(savedInstanceState);
+                toggle.syncState();
+            }
+
+            I dont know what it does but if it does something special let me know
+
+
+
+             */
+
+
+
         });
     }
-
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        toggle.syncState();
-    }
 }
+
+
