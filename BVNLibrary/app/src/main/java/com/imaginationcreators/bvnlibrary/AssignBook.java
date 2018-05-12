ea260bd560ab8d5bb5a8036c91a914884cc45096
Ajay Arumugam <ajayvairam.krishna@gmail.com>
2018-05-11 19:00:08 -0500

    58 Adding Reservation Methods

--- BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/AssignBook.java
+++ BVNLibrary/app/src/main/java/com/imaginationcreators/bvnlibrary/AssignBook.java
@@ -10,6 +10,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.ParseException;
@@ -231,15 +232,44 @@ public class AssignBook {
     }
 
     private void getUserReservedBooks(){
+        final DatabaseReference reserveBookReference = database.getReference().child("Users").child(mAuth.getUid()).child("Holds");
+        final Search search = new Search();
+        search.setLocalDatabaseForSearchTitle();
+        search.dbSource.getTask().addOnCompleteListener(new OnCompleteListener<ArrayList<Books>>() {
+            @Override
+            public void onComplete(@NonNull Task<ArrayList<Books>> task) {
+                reserveBookReference.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        for(Books book : search.searchSample){
+                            if(book.getTitle().equalsIgnoreCase(dataSnapshot.getKey())){
+                                reservedBooks.add(book);
+                            }
+                        }
+                    }
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {}
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
+            }
+        });
+
 
     }
 
     public void reserveBook(Books book){
-        database.getReference().child("Books")
+        database.getReference().child("Users").child(mAuth.getUid()).child("Holds").child(book.getTitle()).setValue("1");
+        database.getReference().child("Books").child("Book").child(book.getTitle()).child("Holds").child(mAuth.getUid()).setValue("1");
     }
 
     // Remove hold from book
     public void removeHold(Books book) {
-        database.getReference().child("Books").child("Book").child(book.getTitle()).child("Holds").child("User Id").child(mAuth.getUid()).removeValue();
+        database.getReference().child("Books").child("Book").child(book.getTitle()).child("Holds").child(mAuth.getUid()).removeValue();
+        database.getReference().child("Users").child(mAuth.getUid()).child("Holds").child(book.getTitle()).removeValue();
     }
 }
